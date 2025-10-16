0b297abea778521ab4ab70ebb8ffb828b398a36b
aaditya2072 <adityadav2072@gmail.com>
2025-10-16 03:31:35 +0000

    2 and 3 done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict"; 
+
+// alert("Hello")
+
+let name = "Aaditya"
+let age = 18;
+let isLoggoedIn = false
+
+undefined is itself a type
+
+null is object type
+console.log(typeof())
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33ajbc";
+ console.log(typeof score);
+ console.log(typeof(score));
+
+let InNum = Number(score);
+ console.log(InNum);
+
+let isLoggedIn = 10;
+console.log(isLoggedIn);
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+
+let num = 33;
+let stringNumber = String (num);
+
+console.log(stringNumber);
+
+console.log(typeof(stringNumber));
