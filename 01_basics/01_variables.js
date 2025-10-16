586bf788b354f20788d1c9232725f7a110c4837a
aaditya2072 <adityadav2072@gmail.com>
2025-10-16 03:30:50 +0000

    basics learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 35343
+let accountEmail = "adit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ktm"
+
+
+console.log(accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
